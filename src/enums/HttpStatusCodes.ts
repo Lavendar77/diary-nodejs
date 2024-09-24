7e6bc8085813fdfd287477ec61c053af2836130a
adeyinka@seamlesshr.com <adeyinka@seamlesshr.com>
2024-09-24 15:07:15 +0100

    complete serverless for diary APIs

--- src/enums/HttpStatusCodes.ts
+++ src/enums/HttpStatusCodes.ts
@@ -2,6 +2,7 @@ export enum HttpStatusCodes {
     Success = 200,
     Created = 201,
     Accepted = 202,
+    NoContent = 204,
     BadRequest = 400,
     NotFound = 404,
     Unauthorized = 401,
--- src/handlers/api/diary.ts
+++ src/handlers/api/diary.ts
@@ -1,18 +1,35 @@
-import { APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2, Context, Handler } from "aws-lambda";
+import { APIGatewayProxyStructuredResultV2, Context, Handler } from "aws-lambda";
 import LambdaResponder from "../../actions/LambdaResponder";
+import DiaryService from "../../services/DiaryService";
+import { HttpStatusCodes } from "../../enums/HttpStatusCodes";
+import DiaryDto from "../../dtos/Diary/DiaryDto";
 
 
 
 
 
 export const list: Handler = async (
-    _event: APIGatewayProxyEventV2,
+    _event: any,
     _context: Context
 ): Promise<APIGatewayProxyStructuredResultV2> => {
-    return new LambdaResponder(
-        '',
-        null
-    );
+    try {
+        const { diaries } = await new DiaryService().getAll(
+            JSON.parse(_event.requestContext.authorizer.lambda.extras).user.id
+        );
+
+        return new LambdaResponder(
+            'Diaries fetched successfully',
+            {
+                diaries: diaries,
+            }
+        );
+    } catch (err: any) {
+        return new LambdaResponder(
+            err.message || 'Error',
+            null,
+            HttpStatusCodes.BadRequest
+        )
+    }
 };
 
 
@@ -20,13 +37,32 @@ export const list: Handler = async (
 
 
 export const store: Handler = async (
-    _event: APIGatewayProxyEventV2,
+    _event: any,
     _context: Context
 ): Promise<APIGatewayProxyStructuredResultV2> => {
-    return new LambdaResponder(
-        '',
-        null
-    );
+    const requestBody = JSON.parse(_event.body as any);
+    const diaryDto = new DiaryDto(requestBody.title, requestBody.content);
+
+    try {
+        const diary = await new DiaryService().store(
+            JSON.parse(_event.requestContext.authorizer.lambda.extras).user.id,
+            diaryDto
+        );
+
+        return new LambdaResponder(
+            'Diary stored successfully',
+            {
+                diary: diary.toJSON(),
+            },
+            HttpStatusCodes.Created
+        );
+    } catch (err: any) {
+        return new LambdaResponder(
+            err.message || 'Error',
+            null,
+            HttpStatusCodes.BadRequest
+        );
+    }
 };
 
 
@@ -34,13 +70,29 @@ export const store: Handler = async (
 
 
 export const view: Handler = async (
-    _event: APIGatewayProxyEventV2,
+    _event: any,
     _context: Context
 ): Promise<APIGatewayProxyStructuredResultV2> => {
-    return new LambdaResponder(
-        '',
-        null
-    );
+    console.log(_event)
+    try {
+        const diary = await new DiaryService().find(
+            JSON.parse(_event.requestContext.authorizer.lambda.extras).user.id,
+            Number(_event.pathParameters.id)
+        );
+
+        return new LambdaResponder(
+            'Diary fetched successfully',
+            {
+                diary: diary,
+            }
+        );
+    } catch (err: any) {
+        return new LambdaResponder(
+            err.message || 'Error',
+            null,
+            HttpStatusCodes.BadRequest
+        );
+    }
 };
 
 
@@ -48,13 +100,32 @@ export const view: Handler = async (
 
 
 export const update: Handler = async (
-    _event: APIGatewayProxyEventV2,
+    _event: any,
     _context: Context
 ): Promise<APIGatewayProxyStructuredResultV2> => {
-    return new LambdaResponder(
-        '',
-        null
-    );
+    const requestBody = JSON.parse(_event.body as any);
+    const diaryDto = new DiaryDto(requestBody.title, requestBody.content);
+
+    try {
+        const diary = await new DiaryService().update(
+            JSON.parse(_event.requestContext.authorizer.lambda.extras).user.id,
+            Number(_event.pathParameters.id),
+            diaryDto
+        );
+
+        return new LambdaResponder(
+            'Diary updated successfully',
+            {
+                diary: diary,
+            }
+        );
+    } catch (err: any) {
+        return new LambdaResponder(
+            err.message || 'Error',
+            null,
+            HttpStatusCodes.BadRequest
+        );
+    }
 };
 
 
@@ -62,11 +133,25 @@ export const update: Handler = async (
 
 
 export const destroy: Handler = async (
-    _event: APIGatewayProxyEventV2,
+    _event: any,
     _context: Context
 ): Promise<APIGatewayProxyStructuredResultV2> => {
-    return new LambdaResponder(
-        '',
-        null
-    );
+    try {
+        await new DiaryService().delete(
+            JSON.parse(_event.requestContext.authorizer.lambda.extras).user.id,
+            Number(_event.pathParameters.id)
+        );
+
+        return new LambdaResponder(
+            'Diary deleted successfully',
+            null,
+            HttpStatusCodes.NoContent
+        );
+    } catch (err: any) {
+        return new LambdaResponder(
+            err.message || 'Error',
+            null,
+            HttpStatusCodes.BadRequest
+        );
+    }
 };
